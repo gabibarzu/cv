46ba33588b7e627c7a4ee96cb802e7d81e1a7c56
Gabriel Barzu <Gabriel.Barzu@centric.eu>
2020-09-04 14:46:03 +0300

    add hobbies

---
+++ assets/js/resources.js
@@ -0,0 +1,191 @@
+var languages = {
+    1: "ro",
+    2: "en",
+};
+
+var tranlations = {
+    "[lblProjectsDelivered]": {
+        1: "Proiecte livrate",
+        2: "Projects delivered"
+    },
+    "[lblLinesOfCode]": {
+        1: "Linii de cod",
+        2: "Lines of code"
+    },
+    "[lblDrinkedCoffees]": {
+        1: "Cafele băute",
+        2: "Drinked coffees"
+    },
+    "[lblProgrammingYears]": {
+        1: "Ani de programare",
+        2: "Programming years"
+    },
+    "[lblHomeTitle]": {
+        1: "O scurtă descriere despre mine",
+        2: "A little about me"
+    },
+    "[lblHomeDescription]": {
+        1: "Sunt un bun ascultător, care reușește să învețe lucruri noi. Un coleg foarte sociabil care poate ajuta cu ușurință, abilități dobândite în voluntariate. Datorită intuiției și spiritului de observație, pot depana și depăși majoritatea problemelor întâmpinate.",
+        2: "I am a good listener, who succeeds in learning new things. A very sociable colleague who can easily help, skills acquired in volunteering. Due to the intuition and the spirit of observation, I can troubleshoot and overcome most of the problems encountered."
+    },
+    "[btnDownloadResume]": {
+        1: "Descarcă CV-ul",
+        2: "Download resume"
+    },
+    "[lblExperienceTitle]": {
+        1: "Experiență de muncă",
+        2: "Work experience"
+    },
+    "[lblPeriodWork1]": {
+        1: "Noiembrie 2016 - Iunie 2018",
+        2: "November 2016 – June 2018"
+    },
+    "[lblPeriodWork2]": {
+        1: "Iunie 2018 – Decembrie 2018",
+        2: "June 2018 - December 2018"
+    },
+    "[lblPeriodWork3]": {
+        1: "Decembrie 2018 – Februarie 2020",
+        2: "December 2018 - February 2020"
+    },
+    "[lblPeriodWork4]": {
+        1: "Februarie 2020 – Prezent",
+        2: "February 2020 - Present"
+    },
+    "[lblWorkTitle]": {
+        1: "Cum lucrez?",
+        2: "How I work?"
+    },
+    "[lblWorkFast]": {
+        1: "Rapid",
+        2: "Fast"
+    },
+    "[lblWorkFastDescription]": {
+        1: "Timpi de încărcare rapide și interacțiune fără întârzieri.",
+        2: "Fast load times and lag free interaction."
+    },
+    "[lblWorkResponsive]": {
+        1: "Responsive",
+        2: "Responsive"
+    },
+    "[lblWorkResponsiveDescription]": {
+        1: "Aspectele mele vor funcționa pe orice dispozitiv.",
+        2: "My layouts will work on any device."
+    },
+    "[lblWorkIntuitive]": {
+        1: "Intuitiv",
+        2: "Intuitive"
+    },
+    "[lblWorkIntuitiveDescription]": {
+        1: "Preferință puternică pentru ușor de utilizat.",
+        2: "Strong preference for easy to use."
+    },
+    "[lblWorkDynamic]": {
+        1: "Dinamic",
+        2: "Dynamic"
+    },
+    "[lblWorkDynamicDescription]": {
+        1: "Site-urile web nu trebuie să fie statice.",
+        2: "Websites don't have to be static."
+    },
+    "[lblPortofolio]": {
+        1: "Portofoliu",
+        2: "Portofolio"
+    },
+    "[btnVisitWebsite]": {
+        1: "Vizitați site-ul",
+        2: "Visit website"
+    },
+    "[lblAlyCad]": {
+        1: "Bootstrap 4 cu SMTP și PHP",
+        2: "Bootstrap 4 with SMTP on PHP"
+    },
+    "[lblFlightIt]": {
+        1: "AngularJS cu PHP și Bootstrap 3.3",
+        2: "AngularJS on PHP and Bootstrap 3.3"
+    },
+    "[lblInsertRandom]": {
+        1: "Angular 9",
+        2: "Angular 9"
+    },
+    "[lblMyCity]": {
+        1: "CSS3 cu HTML5 doar front side",
+        2: "CSS3 and HTML5 only front side"
+    },
+    "[lblReactMedia]": {
+        1: "Bootstrap 4 cu Pure JS și Slider.JS",
+        2: "Bootstrap 4 and Pure JS with Slider.JS"
+    },
+    "[lblRentMe]": {
+        1: "Bootstrap 4 cu AngularJS și PHP",
+        2: "Bootstrap 4 with AngularJS on PHP"
+    },
+    "[lblSAD]": {
+        1: "Bootstrap 4 cu PHP predictions",
+        2: "Bootstrap 4 and PHP predictions"
+    },
+    "[lblTimeToGo]": {
+        1: "Pure CSS, HTML cu JavaScript",
+        2: "Pure CSS, HTML and JavaScript"
+    },
+    "[lblWedding]": {
+        1: "Pure HTML cu CSS",
+        2: "Pure HTML and CSS"
+    },
+    "[lblFindMe]": {
+        1: "Găsește-mă pe",
+        2: "Find me on"
+    },
+    "[lblKeepInTouch]": {
+        1: "Păstrăm legătura?",
+        2: "Keep in touch?"
+    },
+    "[lblName]": {
+        1: "Nume",
+        2: "Name"
+    },
+    "[lblEmail]": {
+        1: "Email",
+        2: "Email"
+    },
+    "[lblMessage]": {
+        1: "Mesaj",
+        2: "Message"
+    },
+    "[lblMessagePlaceholder]": {
+        1: "Spune-mi gândurile tale ...",
+        2: "Tell me your thoughts and feelings..."
+    },
+    "[btnSendMessage]": {
+        1: "Trimite mesajul",
+        2: "Send message"
+    },
+    "[lblHobies]": {
+        1: "Hobby-uri",
+        2: "Hobbies"
+    },
+    "[lblBasktball]": {
+        1: "Baschetul",
+        2: "Basktball"
+    },
+    "[lblCofeeMaker]": {
+        1: "Cafeaua",
+        2: "Make coffee"
+    },
+    "[lblGaming]": {
+        1: "Jocurile",
+        2: "Gaming"
+    },
+    "[lblMusic]": {
+        1: "Muzica",
+        2: "Music"
+    },
+    "[lblMovies]": {
+        1: "Filmele",
+        2: "Movies"
+    },
+    "[lblPets]": {
+        1: "Animalele",
+        2: "Pets"
+    },
+};
