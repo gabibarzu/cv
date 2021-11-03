18a51404df58b90185f94099a53c18457a9e2e5b
Barzu Gabriel <barzu.gabriel@yahoo.com>
2021-11-03 10:26:20 +0200

    Add stats dynamic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,111 +1,140 @@
+/**
+ *
+ * @param {*} n
+ * @returns
+ */
+function setProjectsDelivered() {
+  $("#projects-delivered").text($(".project").length);
+}
+function setLinesOfCodes() {
+  $("#lines-of-codes").text($(".project").length * 1893);
+}
+function setCoffees() {
+  var diff = Math.floor((Date.now() - new Date("2016-11-16")) / 86400000);
+  var coffees = diff * 1.8;
+  $("#coffees").text(coffees);
+}
+function setProgrammingYears() {
+  var diff = Math.floor((Date.now() - new Date("2016-11-16")) / 86400000);
+  var years = Math.ceil(diff / 365);
+  $("#programming-years").text(years);
+}
+
+setProjectsDelivered();
+setLinesOfCodes();
+setCoffees();
+setProgrammingYears();
+
 Number.prototype.format = function (n) {
-    var r = new RegExp("\\d(?=(\\d{3})+" + (n > 0 ? "\\." : "$") + ")", "g");
-    return this.toFixed(Math.max(0, Math.floor(n))).replace(r, "$&,");
+  var r = new RegExp("\\d(?=(\\d{3})+" + (n > 0 ? "\\." : "$") + ")", "g");
+  return this.toFixed(Math.max(0, Math.floor(n))).replace(r, "$&,");
 };
 
 $(".count").each(function () {
-    $(this)
-        .prop("counter", 0)
-        .animate(
-            {
-                counter: $(this).text(),
-            },
-            {
-                duration: 10000,
-                step: function (step) {
-                    $(this).text("" + step.format());
-                },
-            }
-        );
+  $(this)
+    .prop("counter", 0)
+    .animate(
+      {
+        counter: $(this).text(),
+      },
+      {
+        duration: 10000,
+        step: function (step) {
+          $(this).text("" + step.format());
+        },
+      }
+    );
 });
 
 window.onscroll = function () {
-    hideLanguages();
+  hideLanguages();
 };
 
 function hideLanguages() {
-    var languages = document.getElementById("languages");
-    var btn = document.getElementById("translate");
-    if (document.documentElement.scrollTop > 1) {
-        btn.style.transform = "translateX(0px)";
-        languages.style.transform = "translateX(200px)";
-    } else {
-        btn.style.transform = "translateX(200px)";
-        languages.style.transform = "translateX(0px)";
-    }
+  var languages = document.getElementById("languages");
+  var btn = document.getElementById("translate");
+  if (document.documentElement.scrollTop > 1) {
+    btn.style.transform = "translateX(0px)";
+    languages.style.transform = "translateX(200px)";
+  } else {
+    btn.style.transform = "translateX(200px)";
+    languages.style.transform = "translateX(0px)";
+  }
 }
 
 function showLanguages() {
-    document.getElementById("languages").style.transform = "translateX(0px)";
-    document.getElementById("translate").style.transform = "translateX(200px)";
+  document.getElementById("languages").style.transform = "translateX(0px)";
+  document.getElementById("translate").style.transform = "translateX(200px)";
 }
 
 function validateEmail(email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
 
 function sendMessage(button) {
-    var sendMessage = true;
+  var sendMessage = true;
 
-    var name = $("#inputName");
-    var email = $("#inputEmail");
-    var message = $("#inputMessage");
-    var button = $(button);
-    var form = $("#form");
-    var formTitle = $("#formTitle");
+  var name = $("#inputName");
+  var email = $("#inputEmail");
+  var message = $("#inputMessage");
+  var button = $(button);
+  var form = $("#form");
+  var formTitle = $("#formTitle");
 
-    if (name.val() == "") {
-        name.parent().addClass("has-error");
-        sendMessage = false;
-    } else {
-        name.parent().removeClass("has-error");
-    }
-    if (email.val() == "" || !validateEmail(email.val())) {
-        email.parent().addClass("has-error");
-        sendMessage = false;
-    } else {
-        email.parent().removeClass("has-error");
-    }
-    if (message.val() == "") {
-        message.addClass("has-error");
-        sendMessage = false;
-    } else {
-        message.removeClass("has-error");
-    }
-    if (sendMessage) {
+  if (name.val() == "") {
+    name.parent().addClass("has-error");
+    sendMessage = false;
+  } else {
+    name.parent().removeClass("has-error");
+  }
+  if (email.val() == "" || !validateEmail(email.val())) {
+    email.parent().addClass("has-error");
+    sendMessage = false;
+  } else {
+    email.parent().removeClass("has-error");
+  }
+  if (message.val() == "") {
+    message.addClass("has-error");
+    sendMessage = false;
+  } else {
+    message.removeClass("has-error");
+  }
+  if (sendMessage) {
+    Email.send({
+      SecureToken: "ef544092-7e06-47c5-bdb4-c4952f5c38fc",
+      To: "gabrielbarzu1994@gmail.com",
+      From: email.val(),
+      Subject: "Personal website form",
+      Body: message.val(),
+    }).then((reponse) => {
+      name.val("");
+      email.val("");
+      message.val("");
 
+      name.parent().removeClass("has-error");
+      email.parent().removeClass("has-error");
+      message.removeClass("has-error");
+
+      form.addClass("section-success");
+      formTitle.text(getTranslation("[lblMessageSent]"));
+      button.text(getTranslation("[btnSendNewMessage]"));
+    });
+
+    $.get(
+      "templates/" + getTranslation("[mailTemplate]") + ".txt",
+      function (html_string) {
+        var body = html_string.replace("{{name}}", name.val());
         Email.send({
-            SecureToken: "ef544092-7e06-47c5-bdb4-c4952f5c38fc",
-            To: 'gabrielbarzu1994@gmail.com',
-            From: email.val(),
-            Subject: "Personal website form",
-            Body: message.val()
-        }).then(
-            reponse => {
-                name.val("");
-                email.val("");
-                message.val("");
-
-                name.parent().removeClass("has-error");
-                email.parent().removeClass("has-error");
-                message.removeClass("has-error");
-
-                form.addClass("section-success");
-                formTitle.text(getTranslation("[lblMessageSent]"));
-                button.text(getTranslation("[btnSendNewMessage]"));
-            }
-        );
-
-        $.get("templates/" + getTranslation("[mailTemplate]") + ".txt", function (html_string) {
-            var body = html_string.replace("{{name}}", name.val());
-            Email.send({
-                SecureToken: "ef544092-7e06-47c5-bdb4-c4952f5c38fc",
-                To: email.val(),
-                From: "gabrielbarzu1994@gmail.com",
-                Subject: getTranslation("[lblThankMessage]"),
-                Body: body
-            });
-        }, 'text');
-    }
+          SecureToken: "ef544092-7e06-47c5-bdb4-c4952f5c38fc",
+          To: email.val(),
+          From: "gabrielbarzu1994@gmail.com",
+          Subject: getTranslation("[lblThankMessage]"),
+          Body: body,
+        });
+      },
+      "text"
+    );
+  }
 }
